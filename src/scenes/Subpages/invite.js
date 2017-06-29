839a9cc326b365e2fb63c74c1238ed2dd03e3f16
chuyin <1276965110@qq.com>
2017-06-29 09:26:15 +0800

    2017/6/29

--- src/scenes/Subpages/invite.js
+++ src/scenes/Subpages/invite.js
@@ -10,8 +10,8 @@ import{
 
 const peruri = "http://cloudapi.famesmart.com";
 const appData = require('./../../components/Ajax')
-
 const pxToDp =require('../responsive/px');
+const Addvisitor =require('./addvisitor');
 
 export default class invite extends Component{
 	constructor(props){
@@ -21,28 +21,13 @@ export default class invite extends Component{
 		}
 	}
 
-	//保存到相册
-	savePhoto(){
-		console.log('保存相册')
-	}
-
-	//分享到微信
-	shareWex(){
-		console.log('分享')
-	}
-	
-	//作废
-	// dropCard(){
-	// 	console.log('作废')
-	// }
-	
-// onPress={()=>this.props.backCtrl(false)}
 	//记录
 	recording(){
 		return(
 			<View style={{flex: 1}}>
 				<View style={{height:pxToDp(86), flexDirection:'row', justifyContent:'space-between', borderBottomColor:'#bebebe', borderBottomWidth: pxToDp(2)}}>
-					<TouchableOpacity style={{width:pxToDp(120), alignItems: 'center', justifyContent: 'center'}} onPress={()=>this.props.backCtrl(false)}>
+					<TouchableOpacity style={{width:pxToDp(120), alignItems: 'center',  flexDirection:'row'}} onPress={()=>this.props.backCtrl(false)}>
+						<Image style={{height:pxToDp(48), width: pxToDp(48)}} source={require('./../../assets/arrow-left.png')} 	resizeMode="contain"></Image>
 						<Text style={{fontSize:pxToDp(30)}}>返回</Text>
 					</TouchableOpacity>
 					<View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
@@ -72,24 +57,39 @@ export default class invite extends Component{
 							<Text style={{fontSize: pxToDp(26), color: '#bbb'}}>有效期至： 2017年3月31日 21:53</Text>
 						</View>
 						<View style={{alignItems:'center', justifyContent:'center'}}>
-							<Text style={{fontSize:pxToDp(40), color: '#b1cd29'}}>已过期</Text>
+							<Text style={{fontSize:pxToDp(40), color: '#a0a0a0'}}>已过期</Text>
 						</View>
 					</View>
 
 				</View>
 				<View>
-					<TouchableOpacity style={{height:pxToDp(86), backgroundColor: '#69bdd0', alignItems:'center', justifyContent:'center'}}>
-						<Text style={{fontSize: pxToDp(38), fontWeitht: '600', color: 'white'}}>+添加新邀请</Text>
+					<TouchableOpacity style={{height:pxToDp(86), backgroundColor: '#69bdd0', alignItems:'center', justifyContent:'center'}} onPress={()=> this._addvisitor(true)}>
+						<Text style={{fontSize: pxToDp(38), color: 'white'}}>+添加新邀请</Text>
 					</TouchableOpacity>
 				</View>
 			</View>
 		)
 	}
 
+	//新增页面控制器
+	_addvisitor(bol){
+		if(bol){
+			this.setState({
+				_page: 'addVisitor'
+			})
+		}else {
+			this.setState({
+				_page: 'recording'
+			})
+		}
+	}
+
 	_choosePage(){
 		if(this.state._page == 'recording'){
 			let page = this.recording();
 			return page;
+		} else if(this.state._page == 'addVisitor'){
+			return <Addvisitor backCtrl={()=>this._addvisitor(false)}/>
 		}
 	}
 
